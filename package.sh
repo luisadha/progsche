f1c4a7b17da4568fe22dea624841c85b8a307925
Luis Adha <40796508+luisadha@users.noreply.github.com>
2023-02-09 23:23:38 +0700

    Add files via upload

---
+++ package.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# contoh skrip yang membutuhkan library progres animasi loading
+# Semua adalah contoh yang dibuat sesimpel mungkin
+
+# Virtual Download package scripts
+
+PACKAGES=(
+  termux-info 
+)
+
+function installPackages() {
+  start_animation "DHCP:" 
+
+
+
+  for PACKAGE in "${PACKAGES[*]}"; do
+sleep 10 #00
+echo "$PACKAGE " &>/dev/null
+    
+    if [[ $? -eq 0 ]]; then
+
+      stop_animation $? || exit 1
+
+    else
+
+      stop_animation $?
+
+    fi
+
+  done
+
+
+}
